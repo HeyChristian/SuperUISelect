7b2939df8f8bc5822f4fb3788340eed94cf5b8d6
Juan Albelo <jalbelo@opstrax.com>
2020-03-13 09:37:07 -0500

    Styling Updates
    * Updated Styles
    * Added inputs for Primary Color, Item Hover Color and Active Filter border Color

--- src/app/custom-dropdown/custom-dropdown.component.ts
+++ src/app/custom-dropdown/custom-dropdown.component.ts
@@ -4,7 +4,8 @@ import {
   Input,
   SimpleChanges,
   OnChanges,
-  forwardRef
+  forwardRef,
+  ElementRef
 } from "@angular/core";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
@@ -52,10 +53,20 @@ export class CustomDropdownComponent
   @Input() sortBy: string; // field for sorting the source
   @Input() minFilterLength: number = 1; // minimum characters for filter
 
-  filter = '';
-  constructor() { }
+  // COLOUR SCHEME
+  @Input() primaryColor: string = '#00507D'; // Primary Color
+  @Input() itemHoverColor: string = '#F8F8F8'; // Highlight Color
+  @Input() filterActiveColor: string = '#B3B3B3'; // Filter Active Color
 
-  ngOnInit() { }
+  filter = '';
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.setProperty('--dropdown-primary-color', this.primaryColor);
+    this.elementRef.nativeElement.style.setProperty('--dropdown-item-hover-color', this.itemHoverColor);
+    this.elementRef.nativeElement.style.setProperty('--dropdown-filter-active-color', this.filterActiveColor);
+  }
 
   writeValue(value: any): void {
     this.value = value;
